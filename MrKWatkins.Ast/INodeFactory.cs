dcf06b2ddf3a468a4ca14373671697880b8e656b
MrKWatkins <mrkwatkins@gmail.com>
2022-08-20 15:55:46 +0100

    Fix signature of INodeFactory<TNode>.Create<T>().

--- MrKWatkins.Ast/INodeFactory.cs
+++ MrKWatkins.Ast/INodeFactory.cs
@@ -1,11 +1,13 @@
 namespace MrKWatkins.Ast;
 
-public interface INodeFactory<out TNode>
+public interface INodeFactory<TNode>
     where TNode : Node<TNode>
 {
     [Pure]
     TNode Create(Type nodeType);
 
     [Pure]
-    TNode Create<T>() => Create(typeof(T));
+    T Create<T>()
+        where T : TNode
+        => (T) Create(typeof(T));
 }
